7440870ff1a252011e45cab565e5d9294c5f775d
Leonid <103331029+LeonidSila@users.noreply.github.com>
2022-11-22 12:43:40 +0300

    DZ_1 is good

---
+++ dz_1/dz_1.py
@@ -0,0 +1,9 @@
+user_input = int(input('Введи число:'))
+
+if user_input == 6:
+    print("Выходной")
+elif user_input == 7:
+    print("Выходной")
+else:
+    user_input <= 5
+    print("Будний день")
